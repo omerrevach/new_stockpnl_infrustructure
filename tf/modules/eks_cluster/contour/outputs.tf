06b87168da3134119e6f5fd595a6fd12cd1904da
root <hairviction@gmail.com>
2025-02-22 19:51:24 +0200

    fixed

--- tf/modules/eks_cluster/contour/outputs.tf
+++ tf/modules/eks_cluster/contour/outputs.tf
@@ -1,9 +1,7 @@
 output "contour_namespace" {
-  description = "The namespace where Contour is installed"
   value       = helm_release.contour.namespace
 }
 
 output "contour_release_name" {
-  description = "The name of the Helm release for Contour"
   value       = helm_release.contour.name
 }
