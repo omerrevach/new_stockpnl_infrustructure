d38da176a92dc5742661324561f6160f085e35b0
omerrevach <hairviction@gmail.com>
2025-04-21 22:32:15 +0300

    latest infra

--- tf/eks_setup_root/vars.tf
+++ tf/eks_setup_root/vars.tf
@@ -6,13 +6,11 @@ variable "name" {
 }
 
 variable "hosted_zone_id" {
-  description = "The ID of the Route53 hosted zone"
   type        = string
-  default     = "Z022564630P941WV72XMM" # Your hosted zone ID for stockpnl.com
+  default     = "Z022564630P941WV72XMM" #  hosted zone ID for stockpnl.com
 }
 
 variable "app_domain_name" {
-  description = "Root domain for the app"
   type        = string
   default     = "stockpnl.com"
 }
--- tf/modules/eks_blueprints_addons/vars.tf
+++ tf/modules/eks_blueprints_addons/vars.tf
@@ -1,35 +1,29 @@
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
   type        = string
 }
 
 variable "cluster_endpoint" {
-  description = "Endpoint of the EKS cluster"
   type        = string
 }
 
 variable "cluster_version" {
-  description = "Kubernetes version of the EKS cluster"
   type        = string
 }
 
 variable "oidc_provider_arn" {
-  description = "ARN of the OIDC provider for the EKS cluster"
   type        = string
 }
 
 variable "vpc_id" {
-  description = "ID of the VPC where EKS is deployed"
   type        = string
 }
 
 variable "region" {
-  description = "AWS region"
   type        = string
 }
 
 variable "check_ingress" {
-  description = "Whether to check for the frontend-ingress existence"
+  description = "to check for the frontend-ingress existence"
   type        = bool
   default     = false
 }
